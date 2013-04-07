0cd3404343f2d4731ac401f69e82adfc208d8c84
karo1205 <rkapeller@gmail.com>
2013-04-07 10:30:20 +0200

    Testmessages added to ReisterMessgeHandler

--- snake-java/common/src/foop/java/snake/common/message/handler/RegisterMessageHandler.java
+++ snake-java/common/src/foop/java/snake/common/message/handler/RegisterMessageHandler.java
@@ -2,7 +2,10 @@ package foop.java.snake.common.message.handler;
 
 import java.net.SocketAddress;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 
+import foop.java.snake.common.board.Board;
 import foop.java.snake.common.message.exception.NoMessageHandlerFoundException;
 import foop.java.snake.common.message.*;
 import foop.java.snake.common.player.PlayerRegistry;
@@ -60,10 +63,63 @@ public class RegisterMessageHandler implements MessageHandlerInterface
         try {
             TCPClient client = clientRegistry.getClient(newAddress);
             client.sendMessage(response);
+            testMessages(client);
+            
             client.close();
         } catch (Exception ex) {
             System.out.println("Couldn\'t send response to \"" + ((InetSocketAddress)newAddress).getHostName()+":"+((InetSocketAddress)newAddress).getPort() + "\".");
             System.out.println(ex.getMessage());
         }
+    }
+    /**
+     * Dummy method to send and test board and PrioChanged
+     * @param client
+     * @throws Exception
+     */
+    private void testMessages(TCPClient client) throws Exception {
+    	MessageInterface response;
+  
+		// Test: send board and prio-Message just for test and fun
+		Player p1 = new Player("Player 1");
+		p1.setID(1);
+		Player p2 = new Player("Player 2");
+		p2.setID(2);
+		Player p3 = new Player("Player 3");
+		p3.setID(3);
+		Player p4 = new Player("Player 4");
+		p4.setID(4);
+		Player p5 = new Player("Player 5");
+		p5.setID(5);
+		List<Player> pl = new ArrayList<Player>();
+		pl.add(p1);
+		pl.add(p2);
+		pl.add(p3);
+		pl.add(p4);
+		pl.add(p5);
+		 
+		response = new PrioChangeMessage(pl);
+		
+		System.out.println("Sending Prio-List");
+		 
+		client.sendMessage(response);
+		 
+		Board board = new Board(30, 30);
+		Byte[][] b = new Byte[][]{
+			{0, 0, (byte)0x20, (byte)0x10, (byte)0x10, 0, 0, 0, 0},
+			{0, 0, 0, 0, (byte)0x10, 0, 0, 0, 0},
+			{0, 0, 0, 0, (byte)0x10, 0, 0, 0, 0},
+			{0, 0, 0, 0, (byte)0x10, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, (byte)0x51, 0},
+			{0, 0, 0, 0, 0, 0, 0, (byte)0x11, 0},
+			{0, 0, 0, 0, 0, 0, 0, (byte)0x11, 0},
+			{0, 0, 0, 0, 0, 0, 0, (byte)0x11, 0},
+			{0, 0, 0, 0, 0, 0, 0, (byte)0x11, 0}
+		};
+		board.setBoard(b);
+		
+		Thread.sleep(1000);
+		System.out.println("Sending Board");
+		response = new BoardMessage(board);
+		client.sendMessage(response);       
     }
 }
